965cd1cb97902e977212a4c00bd4764808172165
Andrey Makuha <amakuha1912@gmail.com>
2019-09-03 12:33:30 +0300

    Add seed for admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(email: 'editor@example.org', password: 'password', password_confirmation: 'password', digest: 1, role: 1)
